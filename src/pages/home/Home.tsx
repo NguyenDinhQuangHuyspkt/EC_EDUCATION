f6668f57e51a226dbf98bd48fd0d53f2483df1c1
Quang Huy <edtech.intern@vus-etsc.edu.vn>
2025-07-14 14:36:46 +0700

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen flex items-center justify-center bg-gray-100 font-quickSand'>
+      Hello
+    </div>
+  )
+}
+
+export default Home
